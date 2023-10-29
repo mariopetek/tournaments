717aa5a496ccba9f2b6bc3a380ef921a512e8970
Mario Petek <mariopetek0@gmail.com>
2023-10-29 12:18:08 +0100

    Stylesheet path fix v4

--- src/app.ts
+++ src/app.ts
@@ -15,7 +15,7 @@ const app = express()
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, '/public')))
 app.use(express.urlencoded({ extended: true }))
 
 const externalUrl = process.env.RENDER_EXTERNAL_URL
